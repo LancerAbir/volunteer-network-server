cd20816416ad7d0e673d102cc4bf0a4fcc611018
Lancer Abir <anjan.o24@gmail.com>
2020-10-05 14:19:20 +0600

    dome

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 //** PORT */
 const port = 6600;
 
+
+
 //** Mother App */
 const app = express();
+
 
 //** Middle Ware */
 const middleware = [
@@ -27,10 +30,16 @@ const middleware = [
 ];
 app.use(middleware);
 
+
+
 //** Root Route */
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+
+// const claster = allEvents
+
 
 //** MongoDB Set Up */
 const client = new MongoClient(uri, {
@@ -42,19 +51,19 @@ client.connect((err) => {
         .db(`${process.env.DB_NAME}`)
         .collection(`${process.env.DB_COLLECTION}`);
     // perform actions on the collection object
-    console.log("Database Has Successfully Connected");
+    console.log("Fake Data Database Has Successfully Connected");
 
-    //** POST --> Insert Data & Save Database  */
+    //** POST --> Insert Data & Save Database (Fake Data) */
     app.post("/addVolunteer", (req, res) => {
-        const volunteer = req.body;
-        volunteerCollection.insertMany(volunteer)
+        const newVolunteer = req.body;
+        volunteerCollection.insertMany(newVolunteer)
         .then((result) => {
             console.log(result.insertedCount);
             res.send(result.insertedCount > 0)
         });
     });
 
-    //** GET --> Show All Data */
+    //** GET --> Show All Fake Data */
     app.get("/volunteers", (req, res) => {
         volunteerCollection.find({})
         .toArray((error, documents) => {
@@ -62,6 +71,75 @@ client.connect((err) => {
         })
     })
 });
+
+
+//** MongoDB Set Up */
+const clientEvents = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+clientEvents.connect((err) => {
+    const volunteerCollection = clientEvents
+        .db(`${process.env.DB_NAME}`)
+        .collection(`${process.env.DB_COLLECTION2}`);
+    // perform actions on the collection object
+    console.log("Events Database Has Successfully Connected");
+
+    //** POST --> Insert Data & Save Database (Events Data) */
+    app.post("/addEvents", (req, res) => {
+        const newEvents = req.body
+        volunteerCollection.insertOne(newEvents)
+        .then( result => {
+            console.log(result.insertedCount);
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+    //** GET --> Show All Events Data */
+    app.get("/events", (req, res) => {
+        const queryEmail = req.query.email
+        volunteerCollection.find({email: queryEmail})
+        .toArray((error, documents) => {
+            res.send(documents)
+        })
+    })
+
+});
+
+
+//** MongoDB Set Up */
+const clientAdmin = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+clientAdmin.connect((err) => {
+    const volunteerCollection = clientAdmin
+        .db(`${process.env.DB_NAME}`)
+        .collection(`${process.env.DB_COLLECTION3}`);
+    // perform actions on the collection object
+    console.log("Admin All User Database Has Successfully Connected");
+
+    //** POST --> Insert Data & Save Database (Events Data) */
+    app.post("/allUser", (req, res) => {
+        const newEvents = req.body
+        volunteerCollection.insertOne(newEvents)
+        .then( result => {
+            console.log(result.insertedCount);
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+    //** GET --> Show All Events Data */
+    app.get("/users", (req, res) => {
+        volunteerCollection.find({})
+        .toArray((error, documents) => {
+            res.send(documents)
+        })
+    })
+
+});
+
+
 
 //** App Listen */
 app.listen(port, () => {
